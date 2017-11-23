2a25a5338f7e8e0ac9ae7b0da28af1fd6c78de0a
Mikołaj Styś <zapominacz@gmail.com>
2017-11-23 09:04:39 +0100

    Initial Commit

---
+++ SimpleNeuralNetwork/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleNeuralNetwork
+//
+//  Created by Mikołaj Styś on 23.11.2017.
+//  Copyright © 2017 Mikołaj Styś. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
